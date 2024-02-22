83c36ef2ccded1d95cc6cb2916bfb31eb471c02f
전성호 <kopasd99@gmail.com>
2024-02-22 20:28:46 +0800

    add Entity

---
+++ src/main/java/jpabook/jpashop/domain/Orders.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+@Getter @Setter
+public class Orders {
+
+    @Id @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems=new ArrayList<>();
+
+    @OneToOne(fetch =FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrders(this);
+    }
+    public void setDelivery(Delivery delivery){
+        this.delivery=delivery;
+        delivery.setOrders(this);
+    }
+}
